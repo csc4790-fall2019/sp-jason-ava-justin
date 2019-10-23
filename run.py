e5029be3f6e5ab5df43d73333952a2d23d7bf978
Justin Myers <jmyers20@villanova.edu>
2019-10-23 18:44:55 -0400

    Worked on APIs

--- run.py
+++ run.py
@@ -106,5 +106,9 @@ def get_Stock_Data(company_id):
         abort(404)
     return jsonify({'company': company })
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 if __name__ == '__main__':
     app.run()(debug=True, host='0.0.0.0')
